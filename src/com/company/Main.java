f4bd49e060bf4d17927aa80465b6e65fba16c0ba
suryaprakash <singhyash2207@gmail.comm>
2021-08-22 11:10:09 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int stops;
+        int fare;
+        stops=0;
+        fare=0;
+        stops=stops+1;
+        fare=fare+5;
+        stops=stops+1;
+        fare=fare+3;
+        stops=stops+1;
+        fare=fare+1;
+        System.out.println(stops);
+        System.out.println(fare);
+    }
+}
